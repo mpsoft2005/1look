7ad5ff2bc0140357bff41fc3476212a4c4dbe977
Ben Xie(謝斌) <ben_xie@HTC.COM>
2016-12-27 16:21:47 +0800

    update to unity 5.5

--- Assets/OVR/Editor/OVRBuild.cs
+++ Assets/OVR/Editor/OVRBuild.cs
@@ -86,7 +86,7 @@ partial class OculusBuildApp
 #if UNITY_5_0 || UNITY_5_1
 		EditorUserBuildSettings.androidBuildSubtarget = MobileTextureSubtarget.ETC2;
 #else
-		EditorUserBuildSettings.androidBuildSubtarget = AndroidBuildSubtarget.ETC2;
+		EditorUserBuildSettings.androidBuildSubtarget = MobileTextureSubtarget.ETC2;
 #endif
 	if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.Android)
         {
